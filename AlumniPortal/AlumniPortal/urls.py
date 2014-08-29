9b0571e4300e93f78a97b7a305035b6342b3c11f
f33n1x <4rch17@gmail.com>
2014-08-29 15:31:54 +0530

    Person contact card, Directory page (in development) and Unique information on event cards on home page

--- AlumniPortal/AlumniPortal/urls.py
+++ AlumniPortal/AlumniPortal/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'AlumniPortal.views.home')
+    url(r'^$', 'AlumniPortal.views.home'),
+    url(r'^directory/$', 'AlumniPortal.views.directory'),
 )
--- AlumniPortal/AlumniPortal/views.py
+++ AlumniPortal/AlumniPortal/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def directory(request):
+    return render(request, 'directory.html')
