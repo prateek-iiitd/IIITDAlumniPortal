600ad83c825443276b233cbf7981bece3ddf0451
sauhard <sauhard11099@iiitd.ac.in>
2014-08-30 04:46:28 +0530

    Feedback

--- AlumniPortal/AlumniPortal/static/alumniportal/js/feedback.js
+++ AlumniPortal/AlumniPortal/static/alumniportal/js/feedback.js
@@ -1,22 +1,20 @@
 /**
+ * Created by sauhard on 30/8/14.
+ */
+/**
  * Created by sauhard on 29/8/14.
  */
 $(document).ready(function () {
-    $(".panel-heading").click(function () {
-        $var1 = $(this).parent().get(0);
-        if ($var1.children[1].style.display == "" || $var1.children[1].style.display == "none") {
-            $(".panel-body").slideUp("fast");
-            $($var1.children[1]).slideDown("fast");
-        }
-    });
+    console.log("JS connecting");
     $("#openfeedback").click(function() {
+        console.log("Clicking");
         $("#black").css("z-index", 999).fadeTo("medium", 0.7);
-        $("#feedback").css("z-index", 1000).slideDown("medium");
+        $("#feedback").css("z-index", 1000).slideDown("fast");
 //        $("#feedback").css("z-index", 1000).fadeTo("medium", 1);
     });
     $("#black").click(function () {
         $("#black").fadeTo("medium", 0).css("z-index", -1);
-        $("#feedback").slideUp("medium").css("z-index", -1);
+        $("#feedback").slideUp("fast").css("z-index", -1);
     });
     $(document).keydown(function(e) {
     // ESCAPE key pressed
