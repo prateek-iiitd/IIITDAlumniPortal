47d3aea3fc843e6a3117e3536c476925d0beef62
sauhard <sauhard11099@iiitd.ac.in>
2014-12-11 17:43:01 +0530

    Admin Panel validation + starting with profile page

--- AlumniPortal/AlumniPortal/static/alumniportal/js/admin_forms.js
+++ AlumniPortal/AlumniPortal/static/alumniportal/js/admin_forms.js
@@ -2,6 +2,18 @@
  * Created by sauhard on 10/11/14.
  */
 $(document).ready(function(){
+    if( document.URL.substr(-22) === "/admin_forms/add_news/" ) {
+        $("#news-tab-panel-body").css("display", "block");
+    }
+    if( document.URL.substr(-23) === "/admin_forms/add_event/" ) {
+        $("#event-tab-panel-body").css("display", "block");
+    }
+    if( document.URL.substr(-22) === "/admin_forms/add_blog/" ) {
+        $("#blog-tab-panel-body").css("display", "block");
+    }
+    if( document.URL.substr(-27) === "/admin_forms/add_directory/" ) {
+        $("#directory-tab-panel-body").css("display", "block");
+    }
     $("#news-tab-panel-head").click(function() {
         $("#news-tab-panel-body").slideToggle("fast");
     });
