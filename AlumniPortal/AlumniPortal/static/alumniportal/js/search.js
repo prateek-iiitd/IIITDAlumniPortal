6170ca0d36373fcc94fc85aa86a47514c841d4ef
Sauhard Gupta <sauhard11099@iiitd.ac.in>
2015-03-05 00:17:49 +0530

    Search Results page - linking to profile page

--- AlumniPortal/AlumniPortal/static/alumniportal/js/search.js
+++ AlumniPortal/AlumniPortal/static/alumniportal/js/search.js
@@ -17,26 +17,38 @@ $(document).ready(function () {
         for (var counter=0; counter<total_count; counter++) {
             var student = response['objects'][counter];
             console.log(student);
-            first_name = student['first_name'];
-            last_name = student['last_name'];
-            // WORKPLACE HERE??? BACKEND NOOBS FFS!
-            graduation_year = student['graduation_year'];
 
             // checking for null values
-            if (!first_name) {
+            id = student['id'];
+            if (!student['first_name']) {
                 first_name = '';
             }
-            if (!last_name) {
+            else {
+                first_name = student['first_name'];
+            }
+            if (!student['last_name']) {
                 last_name = '';
             }
-            if (!graduation_year) {
+            else {
+                last_name = student['last_name'];
+            }
+            // WORKPLACE HERE??? BACKEND NOOBS FFS!
+            if (!student['graduation_year']) {
                 graduation_year = '';
             }
             else {
-                graduation_year = 'Batch '+graduation_year;
+                graduation_year = 'Batch '+student['graduation_year'];
+            }
+            if (!student['profile_photo']) {
+                profile_photo = STATIC_URL+'alumniportal/img/user.jpg';
+            }
+            else {
+                profile_photo = student['profile_photo'];
             }
 
-            $("#search-results-right").append('<div class="row"><div class="col-lg-1"><div class="pic"></div></div><div class="col-lg-10" style="padding: 5px 0 0 25px"><p style="margin-bottom: 0">'+first_name+' '+last_name+'<br>Backend &nbsp;| &nbsp;Noobs<br> '+graduation_year+'</p></div></div><div class="divider-4r"></div>');
+            profile_link = '/test/profile/';
+
+            $("#search-results-right").append('<a href="'+profile_link+'"><div class="row"><div class="col-lg-1"><div class="pic" style="background-image: url('+profile_photo+')"></div></div><div class="col-lg-10" style="padding: 5px 0 0 25px"><p style="margin-bottom: 0">'+first_name+' '+last_name+'<br>Backend &nbsp;| &nbsp;Noobs<br> '+graduation_year+'</p></div></div></a>');
         }
     }
 //    $("#search-results-right").append('<div class="row"><div class="col-lg-1"><div class="pic"></div></div><div class="col-lg-10" style="padding: 5px 0 0 25px"><p style="margin-bottom: 0">Sauhard Gupta<br>Adobe &nbsp;| &nbsp;Senior Scientist<br>Batch 2015</p></div></div><div class="divider-4r"></div>');
